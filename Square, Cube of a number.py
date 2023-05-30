ddce383f898a84ceef7aff122575b17ac7b3145c
Anand Bhat <anandbhat07@gmail.com>
2023-05-30 12:47:30 +0530

    Update Square, Cube of a number.py
    python: better to use exponentiation rather than repeated multiplication.

--- Square, Cube of a number.py
+++ Square, Cube of a number.py
@@ -1,7 +1,7 @@
 n=float(input("Enter the number:"))
-a=n*n
-b=n*n*n
-c=n*n*n*n
+a=n**2
+b=n**3
+c=n**4
 print("The square of the number is",a)
 print("The cube of the number is",b)
 print("The value of the number raised to the power of 4 is",c)
